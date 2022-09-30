04f7664d57a9ff852a35665b2da1505a60b983b2
Luuk <luuk.volkering@gmail.com>
2022-09-30 18:35:11 +0200

    Fixed combat variables to make it a little bit balanced.

--- global_objs.py
+++ global_objs.py
@@ -6,6 +6,6 @@ skeleton_chest_opened = False
 lift_on = False
 
 ragdoll = mobs.Mob("Ragdoll", 5, "Padded bat", 0, False)
-goblin_boss = mobs.Mob("Goblin Leader", 10, "Heavy Axe", 3, False)
-highwaymen = mobs.Mob("Highwaymen", 12, "Knives", 3, False)
-hobgoblins = mobs.Mob("Hobgoblins", 25, "", 20, False)
+goblin_boss = mobs.Mob("Goblin Leader", 10, "Heavy Axe", 2, False)
+highwaymen = mobs.Mob("Highwaymen", 12, "Knives", 2, False)
+hobgoblins = mobs.Mob("Hobgoblins", 18, "Wooden Bats", 5, False)
